2278891c66a601fb86e1704be6983836ab395968
edv576 <31450210+edv576@users.noreply.github.com>
2018-11-14 16:15:27 +0100

    Better camera

---
+++ Painting_Creation_Demo/Assets/Scripts/CapturePainting.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapturePainting : MonoBehaviour {
+
+    public RenderTexture paintingScreen;
+
+	// Use this for initialization
+	void Start () {
+		
+
+
+	}
+
+    IEnumerator FreezeCam()
+    {
+        //yield return null;
+        Camera.main.clearFlags = CameraClearFlags.Nothing;
+        yield return null;
+        Camera.main.cullingMask = 0;
+    }
+
+    void StartFreezing()
+    {
+        StartCoroutine("FreezeCam");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartFreezing();
+
+        }
+
+		
+	}
+}
