ec5f43cfb8f0682b3bad8e81c9dff6a428fa1c3f
edv576 <31450210+edv576@users.noreply.github.com>
2018-11-19 12:07:59 +0100

    Capture panting

--- Painting_Creation_Demo/Assets/Scripts/CapturePainting.cs
+++ Painting_Creation_Demo/Assets/Scripts/CapturePainting.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class CapturePainting : MonoBehaviour {
 
     public RenderTexture paintingScreen;
-    public GameObject bigScreen2;
+    public GameObject screenLayer1;
+    public GameObject screenLayer2;
     MeshRenderer bigScreen2MeshRenderer;
     Camera cam;
     private string screenshotsDirectory = "UnityHeadlessRenderingScreenshots";
@@ -20,7 +21,7 @@ public class CapturePainting : MonoBehaviour {
     void Start () {
 
 
-        bigScreen2MeshRenderer = bigScreen2.GetComponent<MeshRenderer>();
+        bigScreen2MeshRenderer = screenLayer2.GetComponent<MeshRenderer>();
         cam = GetComponent<Camera>();
 
         cam.forceIntoRenderTexture = true;
@@ -82,7 +83,7 @@ public class CapturePainting : MonoBehaviour {
             //screenshotCount++;
             TakeScreenShot();
             ReadPixelsOut("SS_" + screenshotCount + ".png");
-            FreezeCam();
+            //FreezeCam();
             
             //StartFreezing();
 
@@ -100,7 +101,14 @@ public class CapturePainting : MonoBehaviour {
 
             byte[] bytes = resultantImage.EncodeToPNG();
 
-          
+            //Sprite s = Sprite.Create(resultantImage, new Rect(0, 0, cam.targetTexture.width, cam.targetTexture.height), new Vector2(0.0f, 0.0f));
+
+            Texture2D tex = new Texture2D(cam.targetTexture.width, cam.targetTexture.height, TextureFormat.RGB24, false);
+            tex.LoadImage(bytes);
+
+            screenLayer2.GetComponent<Renderer>().material.mainTexture = tex;
+
+
             // save on disk
             var path = screenshotsDirectory + "/" + filename;
             File.WriteAllBytes(path, bytes);
@@ -109,6 +117,55 @@ public class CapturePainting : MonoBehaviour {
         }
     }
 
+    IEnumerator FadePaintingIn()
+    {
+
+        if(screenshotCount == 1)
+        {
+            Color tcolor1;
+            Color tcolor2;
+
+            for (float f = 1.0f; f >= 0; f -= 0.005f)
+            {
+                //tcolor1 = layer1.GetComponent<Renderer>().material.color;
+                //tcolor2 = layer2.GetComponent<Renderer>().material.color;
+                //tcolor1.a = f;
+                //tcolor2.a = 1 - f;
+                ////imageLayer1.color = tcolor1;
+                //layer2.GetComponent<Renderer>().material.color = tcolor2;
+
+                //matEnvironment1 = stages[layer].GetComponent<SkinnedMeshRenderer>().materials;
+                //for (int i = 0; i < matEnvironment1.Length; i++)
+                //{
+                //    tColorEnvironment = matEnvironment1[i].color;
+                //    tColorEnvironment.a = f;
+
+                //    matEnvironment1[i].color = tColorEnvironment;
+                //}
+
+                //matEnvironment2 = stages[layer + 1].GetComponent<SkinnedMeshRenderer>().materials;
+                //for (int i = 0; i < matEnvironment2.Length; i++)
+                //{
+                //    tColorEnvironment = matEnvironment2[i].color;
+                //    tColorEnvironment.a = 1.0f - f;
+
+                //    matEnvironment2[i].color = tColorEnvironment;
+                //}
+
+
+
+                yield return new WaitForSeconds(0.005f);
+
+
+            }
+
+        }
+
+
+        yield return new WaitForSeconds(0.005f);
+    }
+
+
     public void TakeScreenShot()
     {
         screenshotCount += 1;
@@ -116,9 +173,9 @@ public class CapturePainting : MonoBehaviour {
         cam.Render();
         resultantImage = new Texture2D(cam.targetTexture.width, cam.targetTexture.height, TextureFormat.RGB24, false);
 
-        Sprite s = Sprite.Create(resultantImage, new Rect(0, 0, cam.targetTexture.width, cam.targetTexture.height), new Vector2(0.0f, 0.0f));
+        //Sprite s = Sprite.Create(resultantImage, new Rect(0, 0, cam.targetTexture.width, cam.targetTexture.height), new Vector2(0.0f, 0.0f));
 
-        bigScreen2.GetComponent<MeshRenderer>().material.mainTexture = resultantImage;
+        //bigScreen2.GetComponent<MeshRenderer>().material.mainTexture = resultantImage;
 
        
         
